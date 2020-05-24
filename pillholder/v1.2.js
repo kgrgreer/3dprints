13aa5db36e5bfd0fcd200754ae755ae2aada0a82
Kevin Greer <kgrgreer@gmail.com>
2020-05-24 13:51:59 -0400

    Initial check-in.

--- pillholder/v1.2.js
+++ pillholder/v1.2.js
@@ -1,5 +1,9 @@
-const FN = 30/2;
-const H = 20;
+const FN = 130; // 130 for production
+const H = 40;
+const R = 100;
+
+// 0.6mm bottom, so 3 layers at 0.2mm
+// print: 0.2mm, 3 layers top and bottom, 2 or 3 sides
 
 function tube(od, id, h) {
     return cylinder({fn: FN, r: od/2, h: h}).subtract(cylinder({fn: FN, r: id/2, h: h}));
@@ -16,26 +20,26 @@ function tube3(od, id, h) {
 
 function base() {
   var s = union(
-      tube(100, 30, H)
+      tube(R, 30, H)
   );
 
   for ( var i = 0 ; i < 8 ; i++ ) {
-    var hole = cylinder({fn: FN, r: 52/4, h: H}).translate([27.5,0,i ? 2 : 0]);
+    var hole = cylinder({fn: FN, r: 52/4, h: H}).translate([27.5,0,i ? 0.6 : 0]);
     hole = hole.scale([1.2,0.9,1]);
     s = s.subtract(hole.rotateZ(360/8*i));
   }
 
-  var outerRing = torus({ri:1, ro: 100/2, h: 3}).scale([1,1,1.5]).translate([0,0,H-3]);
+  var outerRing = torus({ri:1, ro: R/2, h: 3}).scale([1,1,1.5]).translate([0,0,H-3]);
   s = s.subtract(outerRing);
 
   var innerRing = torus({ri:1, ro: 30/2, h: 3}).scale([1,1,1.5]).translate([0,0,H-3]);
   s = s.subtract(innerRing);
 
   for ( var i = 0 ; i < 8 ; i++ ) {
-    var notch = cylinder({fn: FN, r: 2.5, h: 10}).scale([1,3,1]).translate([98/2,0,H-5]);
+    var notch = cylinder({fn: FN, r: 2.5, h: 10}).scale([1,3,1]).translate([(R-2)/2,0,H-5]);
     s = s.subtract(notch.rotateZ(360/8*(i+0.5)))
     s = s.subtract(cylinder({fn: FN, r: 4.5, h: 50}).translate([83.5/2,0,0]).rotateZ(360/8*(i+0.5)))
-    s = s.subtract(cylinder({fn: FN, r: 1.8, h: 50}).translate([39.5/2,0,0]).rotateZ(360/8*(i+0.5)))
+   // s = s.subtract(cylinder({fn: FN, r: 1.8, h: 50}).translate([39.5/2,0,0]).rotateZ(360/8*(i+0.5)))
   }
 
   for ( var i = 0 ; i < 8 ; i++ ) {
@@ -44,18 +48,18 @@ function base() {
     s = s.subtract(notch.rotateZ(360/8*(i+0.5)))
   }
 
-  s = s.subtract(tube(100,99,1.5));
+  s = s.subtract(tube(R,R-1,1.5));
   return s;
 }
 
 function top() {
   var t = union(
-    cylinder({fn: FN, r: 102/2, h: 2}),
-    tube(32,30.3,10),
-    tube(102,100.3,10)
+    cylinder({fn: FN, r: (R+3)/2, h: 0.6}),
+    tube(28.5,27.5,10),
+    tube(R+3,R+1,10)
     );
 
-  t = t.subtract(cylinder({fn: FN, r: 30.3/2, h: 2}))
+  t = t.subtract(cylinder({fn: FN, r: 28/2, h: 2}))
 
   var hole = cylinder({fn: FN, r: 52/4, h: H}).translate([27.5,0,0]);
   hole = hole.scale([1.2,0.9,1]);
@@ -65,6 +69,6 @@ function top() {
 }
 
 function main() {
-   //return top();
+  return top();
   return base();
 }
