8d307eba0a17c9ea25e8453767b2f96d270519d5
Kevin Greer <kgrgreer@gmail.com>
2020-08-31 20:29:17 -0400

    Updated.

--- kayakplug/kayakplug.js
+++ kayakplug/kayakplug.js
@@ -1,13 +1,18 @@
-const R = 20;
-const H = 40;
-const FN = 100;
+const R = 29/2/1.02+1.5;
+const H = 25; // 40
+const FN = 120;
 const CAP_H = 3;
 
 function main () {
-    var handle = cylinder({r:R, h: 5, fn: FN}).rotateX(90).translate([0,2.5,3.35*H]).scale([1,1,0.3])
-    var plug = cylinder({r:R, h:H, fn: FN});
-    var cap = cylinder({r:R+5, h: CAP_H, fn: FN}).translate([0,0,H-CAP_H]);
-    var hole = cylinder({r:3, h: 100}).rotateX(90).translate([0,50,H+2]);
+    var handle = cylinder({r:R*1.3, h: 5, fn: FN}).rotateX(90).translate([0,2.5,3.1*H]).scale([0.5,1,0.32])
+    var plug = cylinder({r:R-2, h:H, fn: FN});
+    for ( var i = 1 ; i < 10 ; i++ ) {
+        var p = i/9;
+        plug = plug.union(cylinder({r:R-1.5*p, h:H*p, fn: FN}));
+    }
+    plug = plug.scale([1,1,-1]).translate([0,0,H]);
+    var cap = cylinder({r:R+0.2, h: CAP_H, fn: FN}).translate([0,0,H-CAP_H]);
+    var hole = cylinder({r:2.5, h: 100}).rotateX(90).translate([0,50,H+2]);
     plug = plug.union(handle);
     plug = plug.union(cap)
     plug = plug.subtract(hole)
