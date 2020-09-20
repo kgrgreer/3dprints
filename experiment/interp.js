4b54e3c01d86cbaa5f4762e56dc48e1f1e8df47e
Kevin Greer <kgrgreer@gmail.com>
2020-09-20 19:11:48 -0400

    Updated.

--- experiment/interp.js
+++ experiment/interp.js
@@ -1,17 +1,17 @@
 function interp(s1, e1, s2, e2) {
     return function(v) {
+        if ( v < s1 || v > e1 ) return v;
         var p = (v - s1) / (e1 - s1);
         return s2 + p * (e2 - s2);
     }
 }
 
 function main () {
-    var s = sphere({r:1});
+  var s = sphere({r:1});
   s.polygons.forEach(p => {
     p.vertices.forEach(v => {
       var pos = v.pos;
-      if ( pos._z > 0.8 )
-        pos._z = interp(0.8,1,0.8,10)(pos._z);
+        pos._z = interp(0,1,0,5)(pos._z);
     })
   });
   return s;
