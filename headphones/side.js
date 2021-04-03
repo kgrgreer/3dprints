626726732ebf43f625b78fb1c215540b1cd610e1
Kevin Greer <kgrgreer@gmail.com>
2021-04-03 19:26:12 -0400

    Updated.

--- headphones/side.js
+++ headphones/side.js
@@ -1,11 +1,12 @@
 const e  = 0.5
-const RO = 3;
-const RI = 2;
+const RO = 3+e;
+const RI = 2+e/2;
 const H1 = 5.2;
-const H2 = 2;
-const H3 = 1.5;
+const H2 = 2-e;
+const H3 = 1.5+2*e;
 const FN = 60;
 const RS = 4.5+e;
+const HR = 3.35/2;
 
 const BLACK = [0,0,0];
 // 7.5 exposed band
@@ -49,12 +50,12 @@ function createText(m) {
 
 function createArm() {
   var a = [];
-  for ( var i = 0 ; i < 1 ; i+= 0.01 ) {
+  for ( var i = 0 ; i < 1.2 ; i+= 0.01 ) {
      var s = cube({size:[11,8-2*i,0.5], radius:0.25});
      if ( i > 0.15 ) {
          s = s.subtract(cube({size:[6,1,3]}).translate([2.5,2.5+(1-i),0]));
      }
-         s = s.translate([0,-1,45*i]).rotateX(24*i);
+         s = s.translate([0,-1,45*i]).rotateX(18*i);
      a.push(s);
   }
 
@@ -79,8 +80,8 @@ function main() {
       cylinder({r:RS, h:H1+H2+H3, fn: FN})
       ));
 
-  s = s.union(s.rotateZ(90)).rotateZ(90);
-  // return s;
+  s = s.union(s.rotateZ(90)).rotateZ(-90);
+  //return s;
 
   var b = cube({size:[11,12,12], radius:0.5, center:[true,true,false]});
 
@@ -88,19 +89,21 @@ function main() {
 
   var arm = createArm();
   b = b.union(arm.translate([-5.5,-3,8.5]))
-  // return b;
+  //return b;
 
   b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,4,10.6]))
-  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-50,4,10.6]))
+  b = b.subtract(cylinder({r:HR,h:100, fn: FN}).rotateY(90).translate([-50,4,10.6]))
   b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,-4,10.6]))
-  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-50,-4,10.6]))
+  b = b.subtract(cylinder({r:HR,h:100, fn: FN}).rotateY(90).translate([-50,-4,10.6]))
 
-  b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,-12.5,48]))
-  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-5.5,-12.5,48]))
+  b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,-12.5,55]))
+  b = b.subtract(cylinder({r:HR,h:100, fn: FN}).rotateY(90).translate([-5.5,-12.5,55]))
 
-return b;
+  var b2 = b.subtract(cube({size:[100,100,100]}).translate([0,-50,0]))
+  var b3 = b.intersect(cube({size:[100,100,100]}).translate([0,-50,0]))
 
-  b = b.subtract(cube({size:[100,100,100]}).translate([0,-50,0]))
-//  return b;
-  return b.rotateY(-90);
+  b2 = b2.rotateY(-90);
+  b3 = b3.rotateY(-90);
+
+  return b2.union(b3.translate([50,30,0]).rotateY(180))
 }
