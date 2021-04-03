b58d9ea0e263c1c726abfdf72e5ecfe5304c3904
Kevin Greer <kgrgreer@gmail.com>
2021-04-03 15:37:11 -0400

    Updated.

--- headphones/side.js
+++ headphones/side.js
@@ -4,12 +4,71 @@ const RI = 2;
 const H1 = 5.2;
 const H2 = 2;
 const H3 = 1.5;
-const FN = 30;
+const FN = 60;
 const RS = 4.5+e;
 
+const BLACK = [0,0,0];
 // 7.5 exposed band
 
+function createText(m) {
+    if ( typeof m === 'string' ) m = { text: m };
+
+    return Object.assign({
+      text: 'A',
+      w: 5,
+      h: 8,          // depth
+      scale: 0.15,
+      justify: 'L',  // justification: R, C or defaults to Left
+      a: 0,          // angle of rotation
+      color: BLACK,
+      toSolid: function() {
+        var o = [];
+        var l = vector_text(0, 0, this.text);
+
+        l.forEach(s => o.push(rectangular_extrude(s, {w: this.w, h: this.h})));
+
+        var txt = union(o).setColor(this.color).scale([this.scale, this.scale, 1]);
+
+        if ( this.a ) txt = txt.rotateZ(this.a);
+
+        var bounds = txt.getBounds();
+        if ( this.justify === 'R' ) {
+          txt = txt.translate([-bounds[1].x, -bounds[0].y, -2.5]);
+        } else if ( this.justify === 'C' ) {
+          txt = txt.translate([-bounds[1].x/2, -bounds[0].y, -2.5]);
+        } else {
+//          txt = txt.translate([-bounds[0].x, -bounds[0].y, -2.5]);
+          txt = txt.translate([0, -bounds[0].y, -2.5]);
+          }
+
+        return txt;
+      }
+    }, m);
+}
+
+
+function createArm() {
+  var a = [];
+  for ( var i = 0 ; i < 1 ; i+= 0.01 ) {
+     var s = cube({size:[11,8-2*i,0.5], radius:0.25});
+     if ( i > 0.15 ) {
+         s = s.subtract(cube({size:[6,1,3]}).translate([2.5,2.5+(1-i),0]));
+     }
+         s = s.translate([0,-1,45*i]).rotateX(24*i);
+     a.push(s);
+  }
+
+  var arm = union.apply(null, a);
+
+  var r = createText({text:'R', justify:'C'}).toSolid();
+  r = r.rotateX(90).rotateZ(180).translate([5.5,7.5,5]);
+
+  arm = arm.subtract(r);
+  return arm;
+}
+
 function main() {
+
   var s = cylinder({r:RI, h:H1+H2+H3, fn: FN})
 
   s = s.union(cylinder({r:RO, h:H1, fn: FN}) )
@@ -25,19 +84,21 @@ function main() {
 
   var b = cube({size:[11,12,12], radius:0.5, center:[true,true,false]});
 
-  b = b.subtract(cylinder({r:1,h:100, fn: FN}).rotateY(90).translate([-50,4,10]))
-  b = b.subtract(cylinder({r:1,h:100, fn: FN}).rotateY(90).translate([-50,-4,10]))
-
   b = b.subtract(s);
 
-  var arm = cube({size:[11,6,50], radius:1});
-  arm = arm.subtract(cube({size:[6,1,40]}).translate([2.5,2.5,10]));
-  b = b.union(arm.rotateX(22).translate([-5.5,-3,8.5]))
+  var arm = createArm();
+  b = b.union(arm.translate([-5.5,-3,8.5]))
   // return b;
 
-  b = b.subtract(cylinder({r:1,h:100, fn: FN}).rotateY(90).translate([-50,4,10]))
-  b = b.subtract(cylinder({r:1,h:100, fn: FN}).rotateY(90).translate([-50,-4,10]))
+  b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,4,10.6]))
+  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-50,4,10.6]))
+  b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,-4,10.6]))
+  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-50,-4,10.6]))
 
+  b = b.union(cylinder({r:4.8/2,h:11, fn: FN}).rotateY(90).translate([-5.5,-12.5,48]))
+  b = b.subtract(cylinder({r:3.1/2,h:100, fn: FN}).rotateY(90).translate([-5.5,-12.5,48]))
+
+return b;
 
   b = b.subtract(cube({size:[100,100,100]}).translate([0,-50,0]))
 //  return b;
