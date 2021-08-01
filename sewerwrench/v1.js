303406f0e97528089b34095e0fd479d4a9db172c
Kevin Greer <kgrgreer@gmail.com>
2021-08-01 15:39:22 -0400

    Updated.

--- sewerwrench/v1.js
+++ sewerwrench/v1.js
@@ -1,15 +1,15 @@
-const R1 = 110/2;
-const R2 = 120/2;
+const R1 = 94/2;
+const R2 = 104/2;
 const H  = 12;
 
 function main() {
-  var s = cylinder({h:H, r: R2})
+  var s = cylinder({h:H, r: R2, fn: 100})
     s = s.union(cube({size:[20,140,H]}).translate([-10,0,0]))
 
-    s = s.subtract(cylinder({h:H, r: R1}));
+    s = s.subtract(cylinder({h:H, r: R1, fn: 100}));
 
     for ( var i = 0 ; i < 4 ; i++ )
-      s = s.union(cube({size:[10,24,H]}).translate([R1-10,0,0]).rotateZ(i/4*360));
+      s = s.union(cube({size:[10,22,H]}).translate([R1-10,0,0]).rotateZ(i/4*360));
 
     return s;
 }
