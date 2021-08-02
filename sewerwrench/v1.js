5e2c59aeee6f1907a48438509497b2debd0b2fd3
Kevin Greer <kgrgreer@gmail.com>
2021-08-02 09:26:49 -0400

    Updated.

--- sewerwrench/v1.js
+++ sewerwrench/v1.js
@@ -4,12 +4,15 @@ const H  = 10;
 
 function main() {
   var s = cylinder({h:H, r: R2, fn: 100})
-    s = s.union(cube({size:[20,148,H]}).translate([-10,0,0]))
 
+    // handle
+    s = s.union(cube({size:[20,160,H], radius: 2}).translate([-10,0,0]))
+    s = s.union(cylinder({r:10, h:H}).intersect(sphere({r:10.7}).translate([0,0,5])).translate([0,R2+90,0]))
+    s = s.subtract(cylinder({r:2.5, h:H}).translate([0,R2+90,0]))
     s = s.union(cube({size:[30,R2+5,H]}).translate([-15,0,0]))
 
-//    s = s.subtract(cylinder({h:2*H, r: 10, fn: 100}).translate([20,R2+8,0]))
-//    s = s.subtract(cylinder({h:2*H, r: 10, fn: 100}).translate([-20,R2+8,0]))
+    s = s.subtract(cylinder({h:2*H, r: 10, fn: 100}).translate([19.8,R2+7,0]))
+    s = s.subtract(cylinder({h:2*H, r: 10, fn: 100}).translate([-19.8,R2+7,0]))
 
     s = s.subtract(cylinder({h:H, r: R1-1.5, fn: 100}));
 
@@ -17,5 +20,5 @@ function main() {
       s = s.union(cube({size:[10,32,H]}).translate([R1-10,0,0]).rotateZ((i+0.5)/4*360));
     }
 
-    return s.rotateZ(90);
+    return s.rotateZ(90+45);
 }
