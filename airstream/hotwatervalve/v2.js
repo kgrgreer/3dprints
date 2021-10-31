c91163796bc83f792dd0796fc628138f358709db
Kevin Greer <kgrgreer@gmail.com>
2021-10-31 19:12:12 -0400

    Final tested version.

--- airstream/hotwatervalve/v2.js
+++ airstream/hotwatervalve/v2.js
@@ -1,12 +1,13 @@
 // 1" socket for Airstream hotwater heater valve
+// final version, tested
 
 var R1 = 30/2;
-var R2 = 12.5/2;
+var R2 = 25.4/2/Math.sqrt(2);
 
 function main() {
     var s = cylinder({r: R1+2, h: 20, fn: 36})
 
-    s = s.subtract(cylinder({r: R2, h: 30, fn: 4}));
+    s = s.subtract(cube({size:[10,10,50], center:true}));
 
     s = s.subtract(cylinder({r: R1, h: 10, fn: 6}));
 
