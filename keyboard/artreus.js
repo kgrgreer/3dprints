4eaf223456e53e72bba8542c6d156f497d4a322c
Kevin Greer <kgrgreer@gmail.com>
2021-11-21 09:37:57 -0500

    Updates.

--- keyboard/artreus.js
+++ keyboard/artreus.js
@@ -1,9 +1,13 @@
+// based on: https://shop.keyboard.io/products/keyboardio-atreus?variant=31234605809737
+
 
 var A  = 10;  // Key row slant angle
 var FT = 1.5; // Faceplate thickness
-var H  = 20;
-var RW = 18.5;  // Row Width
+var H  = 15;
+var RW = 18.25;  // Row Width
 var RS = 8.6;     // Row Start
+var SW = 14;      // Switch Width 14
+var KW = 17;      // Key Width
 
 function key(s, x, y) {
     var c = cube({size:[15,15,100]}).rotateZ(-A).translate([x,y,0]);
@@ -14,23 +18,26 @@ function key(s, x, y) {
 function row(s, x, y, opt_rows) {
 
   for ( var i = 0 ; i < (opt_rows || 4) ; i++ ) {
-    var c = cube({size:[13,13,100]}).translate([0, 19*i,0]).rotateZ(-A).translate([x,y,0]);
+    var c = cube({size:[SW,SW,100], center: true}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,0]);
     s = s.subtract(c);
+    var key = cube({size:[KW,KW,8], center: [true,true,false]}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,6]);
+    key = key.setColor(i == 2 && y != -5 ? [1,1,1] : [0.2,0.2,0.2]);
+    s = s.union(key);
   }
 
   return s;
 }
 
 function base(keys) {
-var p = polygon({ points: [ [3,6],/*[15,82]*/,[13,78], [17, 82], [220,82],[233,0],[140,-10],[90,-10], [8,0] ] });
-var base = p.extrude().scale([1,1,FT])
+var p = polygon({ points: [ [3,6],/*[15,82]*/,[16,78], [21, 82], [220,82],[233,0],[140,-10],[90,-10], [8,0] ] });
+var base = p.extrude().scale([1,1,FT]).setColor([0.4,0.4,0.4])
 var s = base;
 
  if ( keys ) {
  s = row(s, RS, 7);
  s = row(s, RS+RW, 7);
- s = row(s, RS+RW*2, 9);
- s = row(s, RS+RW*3, 3);
+ s = row(s, RS+RW*2, 8);
+ s = row(s, RS+RW*3, 2);
  s = row(s, RS+RW*4, -5);
  s = row(s, RS+RW*5, -4, 2);
  }
@@ -55,6 +62,7 @@ if ( ! keys ) {
 function main() {
   var s= base(false).setColor([1,1,1]);
 
-  s = s.union(base(true).setColor([0.5,0.5,0.5]).translate([0,0,H]));
+  s = s.union(base(true).translate([0,0,H]));
+  s = s.rotateZ(10)
   return s;
 }
