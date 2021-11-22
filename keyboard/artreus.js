1bc0626b3429d9f3fe5cf9ada459a964b4408f69
Kevin Greer <kgrgreer@gmail.com>
2021-11-21 22:22:57 -0500

    Fix bug which lost keys.

--- keyboard/artreus.js
+++ keyboard/artreus.js
@@ -5,7 +5,7 @@ var A  = 10;  // Key row slant angle
 var FT = 1.5; // Faceplate thickness
 var H  = 15;
 var RW = 18.25;  // Row Width
-var RS = 9.5;     // Row Start
+var RS = 9.5-122;     // Row Start
 var SW = 14;      // Switch Width 14
 var KW = 17;      // Key Width
 var SR = 2;       // screw radius
@@ -16,13 +16,15 @@ function key(s, x, y) {
     return s;
 }
 
-function row(s, x, y, opt_rows) {
+function row(s, x, y, rows, reverse) {
 
-  for ( var i = 0 ; i < (opt_rows || 4) ; i++ ) {
+  for ( var i = 0 ; i < rows ; i++ ) {
     var c = cube({size:[SW,SW,100], center: true}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,0]);
+    if ( reverse ) c = c.scale([-1,1,1])
     s = s.subtract(c);
     var key = cube({size:[KW,KW,8], center: [true,true,false]}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,6]);
     key = key.setColor(i == 2 && y != -5 ? [1,1,1] : [0.2,0.2,0.2]);
+    if ( reverse ) key = key.scale([-1,1,1])
     s = s.union(key);
   }
 
@@ -34,19 +36,37 @@ var p = polygon({ points: [ [9,19],/*[15,82]*/,[19,78], [21, 82], [220,82],[233,
 var base = p.extrude().scale([1,1,FT]).setColor([0.4,0.4,0.4])
 var s = base;
 
- if ( keys ) {
- s = row(s, RS, 7);
- s = row(s, RS+RW, 7);
- s = row(s, RS+RW*2, 8);
- s = row(s, RS+RW*3, 2);
- s = row(s, RS+RW*4, -5);
- s = row(s, RS+RW*5, -4, 2);
- }
 
  // reflect halves
  s = s.intersect(cube({size:[122,500,100]}).translate([0,-250,0]));
  s = s.translate([-122,0,0]);
  s = s.union(s.scale([-1,1,1]));
+
+ if ( ! asBase ) {
+ s = s.translate([0,-41,0]);
+  s = s.intersect(s.scale([0.9875,0.97,1]));
+ s = s.translate([0,41,0]);
+ }
+
+ if ( keys ) {
+ s = row(s, RS, 7, 4);
+ s = row(s, RS, 7, 4, true);
+
+ s = row(s, RS+RW, 7, 4);
+ s = row(s, RS+RW, 7, 4, true);
+
+ s = row(s, RS+RW*2, 8, 4);
+ s = row(s, RS+RW*2, 8, 4, true);
+
+ s = row(s, RS+RW*3, 2, 4);
+ s = row(s, RS+RW*3, 2, 4, true);
+
+ s = row(s, RS+RW*4, -5, 4);
+ s = row(s, RS+RW*4, -5, 4, true);
+
+ s = row(s, RS+RW*5, -4, 2);
+ s = row(s, RS+RW*5, -4, 2, true);
+ }
 
  s = s.translate([0,-41,0]);
 
@@ -64,7 +84,6 @@ function main() {
   var bottom = base(false, true).setColor([1,1,1]);
   var lid    = base(true, false);
 
-  lid = lid.intersect(base(false,false).scale([0.9875,0.97,1]));
   lid = lid.translate([0,0,H-FT]);
 
   // Add screw hold and post
