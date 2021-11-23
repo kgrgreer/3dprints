248e6226cc481240c6ee9dc0ff9cb061f024977d
Kevin Greer <kgrgreer@gmail.com>
2021-11-22 19:58:45 -0500

    Updated.

--- keyboard/artreus.js
+++ keyboard/artreus.js
@@ -2,13 +2,14 @@
 
 
 var A  = 10;  // Key row slant angle
-var FT = 1.5; // Faceplate thickness
+var FT = 1.49; // Faceplate thickness
 var H  = 15;
 var RW = 18.25;  // Row Width
 var RS = 9.5-122;     // Row Start
-var SW = 14;      // Switch Width 14
+var SW = 14 +1 ;      // Switch Width 14, plus 1, for some reason
 var KW = 17;      // Key Width
 var SR = 1.5;       // screw radius
+var KEYS = false;
 
 function key(s, x, y) {
     var c = cube({size:[15,15,100]}).rotateZ(-A).translate([x,y,0]);
@@ -20,12 +21,14 @@ function row(s, x, y, rows, reverse) {
 
   for ( var i = 0 ; i < rows ; i++ ) {
     var c = cube({size:[SW,SW,100], center: true}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,0]);
-    if ( reverse ) c = c.scale([-1,1,1])
+    if ( reverse ) c = c.scale([-1,1,1]);
     s = s.subtract(c);
-    var key = cube({size:[KW,KW,8], center: [true,true,false]}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,6]);
-    key = key.setColor(i == 2 && y != -5 ? [1,1,1] : [0.2,0.2,0.2]);
-    if ( reverse ) key = key.scale([-1,1,1])
-    s = s.union(key);
+    if ( KEYS ) {
+        var key = cube({size:[KW,KW,8], center: [true,true,false]}).translate([7, 19*i+7+y,0]).rotateZ(-A).translate([x,0,6]);
+        key = key.setColor(i == 2 && y != -5 ? [1,1,1] : [0.2,0.2,0.2]);
+        if ( reverse ) key = key.scale([-1,1,1]);
+        s = s.union(key);
+    }
   }
 
   return s;
@@ -36,21 +39,20 @@ var p = polygon({ points: [ [9,19],/*[15,82]*/,[19,78], [21, 82], [220,82],[233,
 var base = p.extrude().scale([1,1,FT]).setColor([0.4,0.4,0.4])
 var s = base;
 
-
  // reflect halves
  s = s.intersect(cube({size:[122,500,100]}).translate([0,-250,0]));
  s = s.translate([-122,0,0]);
  s = s.union(s.scale([-1,1,1]));
 
  if ( ! asBase ) {
- s = s.translate([0,-41,0]);
+  s = s.translate([0,-41,0]);
   s = s.intersect(s.scale([0.9875,0.97,1]));
- s = s.translate([0,41,0]);
+  s = s.translate([0,41,0]);
  }
 
  if ( keys ) {
- s = row(s, RS, 7, 4);
- s = row(s, RS, 7, 4, true);
+   s = row(s, RS, 7, 4);
+   s = row(s, RS, 7, 4, true);
 
  s = row(s, RS+RW, 7, 4);
  s = row(s, RS+RW, 7, 4, true);
@@ -61,8 +63,8 @@ var s = base;
  s = row(s, RS+RW*3, 2, 4);
  s = row(s, RS+RW*3, 2, 4, true);
 
- s = row(s, RS+RW*4, -5, 4);
- s = row(s, RS+RW*4, -5, 4, true);
+ s = row(s, RS+RW*4, -4, 4);
+ s = row(s, RS+RW*4, -4, 4, true);
 
  s = row(s, RS+RW*5, -4, 2);
  s = row(s, RS+RW*5, -4, 2, true);
@@ -100,8 +102,9 @@ function main() {
     bottom = bottom.subtract(cylinder({r:3, h:100}).rotateX(90).translate([-90+i*18,100,H]));
     bottom = bottom.subtract(cylinder({r:3, h:100}).rotateX(90).translate([-90+i*18,100,H-FT]));
   }
+  return lid;
   var s = bottom;
-  //s = s.union(lid);
+ // s = s.union(lid);
    s = bottom;
 
   s = s.rotateZ(10);
