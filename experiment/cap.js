6020d83c1fcc004b45e1eaae578004a7264af04d
Kevin Greer <kgrgreer@gmail.com>
2021-12-06 22:01:35 -0500

    Updated.

--- experiment/cap.js
+++ experiment/cap.js
@@ -1,10 +1,22 @@
-function interp(d1, d2, r1, r2, v) {
-    return r1 + (r2-r1) * (v-d1)/(d2-d1);
-}
+const TEXT = true;
 
-const H = 9;
-const W = 17;
-const R = 35;
+const H  = 9;
+const W  = 16;
+const W2 = 17;
+const R  = 35;
+
+/*
+Print Instructions:
+
+PLA:
+Temp.: 200, 40, 50
+.1mm layer height
+20mm/s travel speed
+
+PETG:
+.1mm layer height
+
+*/
 
 function createText(m) {
   if ( typeof m === 'string' ) m = { text: m };
@@ -43,68 +55,126 @@ function createText(m) {
 
 
 function stem() {
-        var sw = 6.2; /*make smaller to reduce friction Was: 5.4+1.5*/
-        var sh = 4.8;
-        const W = 4.3; // add 0.2 when testing to make easier to put on and remove
-        const H = 1.4;
-        var D = 8;
-        var stem = cube({radius:0.9, roundradius: 0.9,size:[sh,sw, D+4]}).intersect(cube({size:[sh,sw, D]})).translate([-sh/2,-sw/2,-2]);
-        stem = stem.intersect(cube({size:[20,20,20], center: [true,true,false]}))
-        var hollow = cube({size:[W,H,4]}).translate([-W/2,-H/2,0]).union(cube({size:[H,W,4]}).translate([-H/2,-W/2,0]));
-        return stem.subtract(hollow);
+  var sw = 6; /*make smaller to reduce friction Was: 5.4+1.5*/
+  var sh = 4.8;
+  const W = 4.3; // add 0.2 when testing to make easier to put on and remove
+  const H = 1.5;
+  var D = 8;
+
+  var stem = cube({radius:2, fn: 40, roundradius: 0.9,size:[sh,sw, D+4]}).intersect(cube({size:[sh,sw, D]})).translate([-sh/2,-sw/2,-0.8]);
+  stem = stem.intersect(cube({size:[20,20,20], center: [true,true,false]}))
+
+  function cross(p, height) {
+    var c = cube({size:[W+p,H+p,height], center: [true,true,false]}).union(cube({size:[H+p,W+p,height], center: [true,true,false]}));
+  stem = stem.subtract(c);
+
+  }
+  cross(0, 4);
+  cross(0.4, 0.1);
+  cross(0.2, 0.2);
+
+  return stem;
 }
 
-function cap(n, e, s, w, config) {
-const W2 = W + 2;
-    var c = cube({fn:50,size:[W,W2,H], center: [true,true,false], xxxradius: 0.5});
-    var o = c.scale([1,1,10]);
 
-    c = c.intersect(o.translate([0,-W2/2,0]).rotateX(n).translate([0,W2/2,0]));
-    c = c.intersect(o.translate([-W/2,0,0]).rotateY(-e).translate([W/2,0,0]));
-    c = c.intersect(o.translate([0,W2/2,0]).rotateX(-s).translate([0,-W2/2,0]));
-    c = c.intersect(o.translate([W/2,0,0]).rotateY(w).translate([-W/2,0,0]));
+function cap(config) {
+    var n, e, s, w;
 
-
-    c = c.intersect(cube({size:[20,20,20], center: [true, true, false] }))
- o = c;
-//    c = c.subtract(c.scale([0.97,0.97,.5]));
-
-   // c = c.subtract(cube({size:[20,20,5], center: [true, true, false] }))
-
-    var cy = cylinder({r:R, h:100, center: true, fn:360}).rotateY(90).translate([0,0,R+H-2]);
-    c = c.subtract(cy);
-
-/*    c = c.expand(0.4,4)
-    c = c.intersect(cube({size:[20,20,20], center: [true, true, false] }))
-*/
-//    c = c.subtract(c.translate([0,0,-2]));
-    c = c.subtract(c.scale([0.95,0.95,0.77]));
-    c = c.union(stem());
-    c = c.subtract(cy);
-    c = c.union(cube({size:[3,17,0.5],center:[true,true,false]}).translate([0,0,5.3]))
-    c = c.intersect(o);
-
-    if ( config.cLabel ) {
-        config.cLabel.justify = 'C';
-        config.cLabel.w = 5;
-      var t = createText(config.cLabel).toSolid().rotateZ(-90).translate([1.5,4.5,H]);
-      t = t.subtract(c);
-      t = t.translate([0,0,-1]).setColor([1,1,1]);
-      c = c.subtract(t);
+    if ( config.thumb ) {
+      [w, n, e, s] = [
+        [4,4,15,14],
+        [15,4,15,14],
+        [15,4,4,14]
+      ][config.row-1];
+    } else {
+      [w, n, e, s] = [
+        [5, 4, 5,12],
+        [5, 16,5,16],
+        [5, 16,5,4]
+      ][config.row-1];
     }
-    return c;
+  var c = cube({fn:50,size:[W,W2,H], center: [true,true,false], xxxradius: 0.5});
+  var o = c.scale([1,1,10]);
+
+  c = c.intersect(o.translate([0,-W2/2,0]).rotateX(n).translate([0,W2/2,0]));
+  c = c.intersect(o.translate([-W/2,0,0]).rotateY(-e).translate([W/2,0,0]));
+  c = c.intersect(o.translate([0,W2/2,0]).rotateX(-s).translate([0,-W2/2,0]));
+  c = c.intersect(o.translate([W/2,0,0]).rotateY(w).translate([-W/2,0,0]));
+
+
+  c = c.intersect(cube({size:[20,20,20], center: [true, true, false] }))
+  o = c;
+
+  var cy = cylinder({r:R, h:100, center: true, fn:360}).rotateX(90).translate([0,0,R+H-2]);
+  c = c.subtract(cy);
+
+  c = c.subtract(c.scale([0.95,0.95,0.77]));
+  c = c.union(stem());
+  c = c.subtract(cy);
+  c = c.union(cube({size:[3,17,0.5],center:[true,true,false]}).translate([0,0,5.3]))
+  c = c.intersect(o);
+
+  if ( TEXT && config.cLabel ) {
+    config.cLabel.justify = 'C';
+    config.cLabel.w = 5;
+    var t = createText(config.cLabel).toSolid().translate([-4,0,H]);
+    t = t.subtract(c);
+    t = t.translate([0,0,-1]).setColor([1,1,1]);
+    c = c.subtract(t);
+  }
+
+  c = c.translate([config.col*20, -config.row*20, 0]);
+
+  return c;
 }
+
 
 function main () {
-  //  return stem();
-   // return cap(5,5,5,5);
-    return union(
-      cap(5,4,5,14,{cLabel: {text: 'Y'}}).translate([20,-20,0]),
-      cap(5,10,5,10,{cLabel:{text: 'H'}}).translate([0,-20,0]),
-      cap(5,14,5,4,{cLabel: {text: 'N'}}).translate([-20,-20,0]),
+  return union(
+    cap({cLabel: {text: 'Q'}, col:1, row: 1}),
+    cap({cLabel: {text: 'A'}, col:1, row: 2}),
+    cap({cLabel: {text: 'Z'}, col:1, row: 3}),
 
-      cap(4,0,15,0,{}).translate([40,40,0]),
-      cap(15,0,15,0,{cLabel:{text: '    Space', scale: 0.14}}).translate([40,20,0]),
-      cap(15,0,4,0,{cLabel: {text: '   Enter', scale: 0.14}}).translate([40,0,0])
-    )
+    cap({cLabel: {text: 'W'}, col:2, row: 1}),
+    cap({cLabel: {text: 'S'}, col:2, row: 2}),
+    cap({cLabel: {text: 'X'}, col:2, row: 3}),
+
+    cap({cLabel: {text: 'E'}, col:3, row: 1}),
+    cap({cLabel: {text: 'D'}, col:3, row: 2}),
+    cap({cLabel: {text: 'C'}, col:3, row: 3}),
+
+    cap({cLabel: {text: 'R'}, col:4, row: 1}),
+    cap({cLabel: {text: 'F'}, col:4, row: 2}),
+    cap({cLabel: {text: 'V'}, col:4, row: 3}),
+
+    cap({cLabel: {text: 'T'}, col:5, row: 1}),
+    cap({cLabel: {text: 'G'}, col:5, row: 2}),
+    cap({cLabel: {text: 'B'}, col:5, row: 3}),
+
+    cap({thumb: true, col:6, row:1 }),
+    cap({thumb: true, col:6, row: 2, cLabel:{text: '    Del', scale: 0.12}}),
+    cap({thumb: true, col:6, row: 3, cLabel: {text: '   Tab', scale: 0.12}}),
+
+    cap({thumb: true, col:7, row:1 }),
+    cap({thumb: true, col:7, row: 2, cLabel:{text: '    Space', scale: 0.12}}),
+    cap({thumb: true, col:7, row: 3, cLabel: {text: '   Enter', scale: 0.12}}),
+
+    cap({cLabel: {text: 'Y'}, col:8, row: 1}),
+    cap({cLabel: {text: 'H'}, col:8, row: 2}),
+    cap({cLabel: {text: 'N'}, col:8, row: 3}),
+
+    cap({cLabel: {text: 'U'}, col:9, row: 1}),
+    cap({cLabel: {text: 'J'}, col:9, row: 2}),
+    cap({cLabel: {text: 'M'}, col:9, row: 3}),
+
+    cap({cLabel: {text: 'O'}, col:10, row: 1}),
+    cap({cLabel: {text: 'L'}, col:10, row: 2}),
+    cap({cLabel: {text: '<,'}, col:10, row: 3}),
+
+    cap({cLabel: {text: 'P'}, col:11, row: 1}),
+    cap({cLabel: {text: ':;'}, col:11, row: 2}),
+    cap({cLabel: {text: '?/'}, col:11, row: 3}),
+
+
+  ).translate([-100,25,0]);
 }
