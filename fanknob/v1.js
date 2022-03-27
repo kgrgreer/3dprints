3e1c8d3958408597e0432b25effa576aabdc9fd6
Kevin Greer <kgrgreer@gmail.com>
2022-03-27 11:06:57 -0400

    Updated.

--- fanknob/v1.js
+++ fanknob/v1.js
@@ -1,17 +1,17 @@
-const R2 = 6.2/2*1.01;
-const R1 = R2+1;
-const H1 = 5;
+const R2 = 6.2/2*1.03;
+const R1 = R2+2;
+const H1 = 7;
 const H2 = 8;
 
 function main() {
-  var s = cylinder({r:R1, h: H1+1});
+  var s = cylinder({r:R1, h: H1+0.5});
   var h1 = cylinder({r:R2, h: H2});
   var h2 = cylinder({r:R2, h: H1});
 
   h1 = h1.subtract(cube({size:[10,10,10], center:[1,0,0]}).translate([0,R2-5/2,0]));
   h2 = h2.subtract(cube({size:[10,10,10], center:[1,0,0]}).translate([0,1,0]));
 
-  s = s.subtract(h2.translate([0,0,-2]));
-  s = s.union(h1.translate([0,0,H1+1]).rotateZ(90))
+  s = s.subtract(h2.translate([0,0,-2]).scale([1.03,1.03,1]));
+  s = s.union(h1.translate([0,0,H1+0.5]).rotateZ(180))
   return s;
 }
