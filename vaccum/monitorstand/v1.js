2fdcf63f456149ea6dfa79eb859a16709cee3d10
Kevin Greer <kgrgreer@gmail.com>
2022-10-26 09:40:10 -0400

    Updated.

--- vaccum/monitorstand/v1.js
+++ vaccum/monitorstand/v1.js
@@ -1,8 +1,15 @@
 const T = 9;
 const a = 10;
+const FN = 136;
 
 function monitor() {
-    return cube({size:[360,10,230], center: [1,1,0]}).translate([0,0,3]).rotateX(10).translate([0,0,3]).setColor([0.4,0.4,0.4]);
+    var s = cube({size:[360,10,230], center: [1,1,0]});
+
+    s = s.subtract(s.rotateX(70).translate([0,10,-7]));
+
+    s = s.translate([0,0,3]).rotateX(10).translate([0,0,3]).setColor([0.4,0.4,0.4]);
+
+    return s;
 }
 
 function holder() {
@@ -16,11 +23,11 @@ function holder() {
 }
 
 function holder2() {
-  var s = cylinder({r:50, h:60}).rotateZ(45).translate([0,-10,0]);
+  var s = cylinder({r:50, h:70, fn: FN}).rotateZ(45).translate([0,-10,0]);
   s = s.subtract(s.scale([0.85,0.85,1]))
-  s = s.union(cube({size:[92,10,8], center:[1,0,0]}))
-  s = s.subtract(cube({size:[100,100,100], center:[1,0,0]}).rotateX(10).translate([0,14,-20]));
-  s = s.subtract(cube({size:[100,100,100], center:[1,0,0]}).rotateX(10).translate([0,0,6]));
+  s = s.union(cube({size:[92,10,8], center:[1,1,0]}))
+  s = s.subtract(cube({size:[100,100,100], center:[1,0,0]}).rotateX(10).translate([0,10,-20]));
+  s = s.subtract(cube({size:[100,100,100], center:[1,0,0]}).rotateX(10).translate([0,0,8]));
 
   s = s.scale([1.2,1.3,1])
   s = s.subtract(cube({size:[200,100,100]}).translate([0,0,10]).rotateX(50).translate([-100,-70,-5]))
@@ -31,6 +38,8 @@ function holder2() {
 function main() {
   var s = holder2();
 
-  s = s.union(monitor());
+  s = s.subtract(monitor());
+
+//  s = s.subtract(cylinder({r:20, h: 200}).rotateY(90).translate([-100,-40,30]).scale([1,1,1]))
   return s;
 }
