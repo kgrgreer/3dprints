33546536fc2e550297ad9ebeeaf534ade077c6b7
Kevin Greer <kgrgreer@gmail.com>
2023-02-27 08:50:42 -0500

    Fix screw holes for real.

--- cameraholder/v4.js
+++ cameraholder/v4.js
@@ -16,7 +16,7 @@ function interp(s, e, p) {
 }
 
 function main() {
-     var s = [];
+  var s = [];
 
   for ( var i = 0 ; i < 20 ; i = (i * 1.02 + 0.1 + i)/2 ) {
     var t = cs(interp(70/2, 45/2, interp(Math.sqrt(i/20), Math.pow(i/20,0.1), i/20)), Math.sqrt((i+0.25)/20.25)).translate([0,0,i*0.98]);
@@ -26,20 +26,17 @@ function main() {
 
   s = union(s);
 
+  s = s.union(s.translate([0,0,-0.25]))
+  s = s.union(s.translate([0,0,-0.5]))
+  s = s.union(s.translate([0,0,-1]))
+  s = s.union(s.translate([0,0,-0.125]))
+
+  s  = s.intersect(cube({size:[100,100,100], center:[1,1,0]}));
+
   s = s.rotateY(-40);
-
-  s = s.rotateY(40);
-
-s = s.union(s.translate([0,0,-0.25]))
-s = s.union(s.translate([0,0,-0.5]))
-s = s.union(s.translate([0,0,-1]))
-s = s.union(s.translate([0,0,-0.125]))
-
-s  = s.intersect(cube({size:[100,100,100], center:[1,1,0]}));
-
-    s = s.subtract(cylinder({r:1, h:100}).translate([0,15,0]));
+  s = s.subtract(cylinder({r:1, h:100}).translate([0,15,0]));
   s = s.subtract(cylinder({r:1, h:100}).translate([0,-15,0]));
-
+  s = s.rotateY(40);
 
   return s;
 }
