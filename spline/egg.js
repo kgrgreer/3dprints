92df1d489c618bfd46d5278fc02c4491c67ef339
Kevin Greer <kgrgreer@gmail.com>
2023-03-19 18:55:46 -0400

    Updated.

--- spline/egg.js
+++ spline/egg.js
@@ -1,35 +1,11 @@
-
 function interp(s, e, p) {
   return s*(1-p)+e*p;
 }
 
-function rspline(t, pairs) {
-    if ( pairs.length == 1 ) {
-      return pairs[0][1];
-    }
-    return interp(
-      rspline(t, pairs.slice(0, pairs.length-1)),
-      rspline(t, pairs.slice(1)),
-      t);
-}
-
 function main() {
-  var r = 3;
-  var d = 30;
   var s = [];
-
-  for ( var i = 0 ; r > 1 && i < 75 ; i++ ) {
-    r = rspline(i/100, [
-      [ 0, 50 ],
-      [ 0.05, 45 ],
-      [ 0.1, 60],
-      [ 0.2, 50],
-      [ 0.3, 60],
-      [1, 0]
-        ]);
-    var c = cylinder({fn:36, r:r, h:1}).translate([0,0,i*1]);
-    s.push(c);
+  for ( var i = 0 ; i < 100 ; i+=0.25 ) {
+      s.push(sphere({fn:36, r:interp(20,12,Math.pow(i/100,2))}).translate([0,0,20+i/5]));
   }
-
   return s;
 }
