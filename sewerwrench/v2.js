0cff567b733b3d0c3adb7101cec59457b96619e8
Kevin Greer <kgrgreer@gmail.com>
2023-06-11 19:17:33 -0400

    Updated.

--- sewerwrench/v2.js
+++ sewerwrench/v2.js
@@ -7,7 +7,7 @@ function main() {
   var s = cylinder({h:H, r: R2, fn: FN})
 
     // handle
-    s = s.union(cube({size:[20,160,H], radius: 5, fn:FN}).translate([-10,0,0]))
+    s = s.union(cube({size:[20,160,H], radius: 4, fn:FN}).translate([-10,0,0]))
     s = s.union(cylinder({r:10, h:H, fn: FN}).intersect(sphere({r:10.7,fn:FN}).translate([0,0,5])).translate([0,R2+90,0]))
     s = s.subtract(cylinder({r:2.5, h:H, fn:FN}).translate([0,R2+90,0]))
     s = s.subtract(sphere({r:4,fn:FN}).translate([0,R2+90,H+2]))
@@ -16,7 +16,7 @@ function main() {
 
     // transition from handle into ring
     for ( var i = 0 ; i <= 1 ; i += 0.025 ) {
-      s = s.union(cube({size:[20 + 12*Math.pow(i,2.5),10,H], center:[1,0,0], radius: (1-i)*5, fn:40-i*20}).translate([0,R1+20*(1-i)-1,0]))
+      s = s.union(cube({size:[20 + 12*Math.pow(i,2.5),10,H], center:[1,0,0], radius: (1-i)*4, fn:40-i*20}).translate([0,R1+20*(1-i)-1,0]))
     }
 
     for ( var i = 0 ; i < 4 ; i++ ) {
