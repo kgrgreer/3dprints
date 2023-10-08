f191ba7dfbdef2d121354ada8105d71d24d3d615
Kevin Greer <kgrgreer@gmail.com>
2023-10-08 16:43:46 -0400

    Updated.

--- lamp/stand.js
+++ lamp/stand.js
@@ -1,9 +1,4 @@
-// title      : OpenJSCAD.org Logo
-// author     : Rene K. Mueller
-// license    : MIT License
-// revision   : 0.003
-// tags       : Logo,Intersection,Sphere,Cube
-// file       : logo.jscad
+const FN = 36;
 
 function interp(s, e, p) {
   return s*(1-p)+e*p;
@@ -20,13 +15,20 @@ function top() {
 
 function main() {
   var s;
-  var pr = 70;
-  var D = 0.1/5;
-  for ( var i = 0 ; i < 1 ; i+=D ) {
-      r = interp(70,10,Math.pow(i,0.2));
-      var c = cylinder({r1:pr, r2:r, h:D*130,fn:8});
-      if ( i > 0.05 )
-        c = c.subtract(cylinder({r1:pr-1, r2:r-1, h:D*130,fn:8}))
+  var pr = 60;
+  var D  = 0.01;
+  for ( var i = 0 ; i < 1 ; i+=D, D = interp(0.01, 0.02, i) ) {
+      r = interp(60,2,Math.pow(i,0.2));
+      if ( i > 0.5 ) r = interp(20,r,Math.pow((1-i)/0.5,0.25))
+      var c = cylinder({r1:pr, r2:r, h:D*132,fn: FN});
+
+      if ( i > 0.05 ) {
+        if ( i > 0.9 ) {
+          c = c.subtract(cylinder({r:4.5, h:D*132}))
+        } else {
+          c = c.subtract(cylinder({r1:pr-2, r2:r-2, h:D*132,fn: FN}))
+        }
+      }
       c = c.translate([0,0,i*130]);
       s = s ? s.union(c) : c;
       pr = r;
@@ -34,5 +36,8 @@ function main() {
 
  // s = s.union(top().translate([0,0,140]));
 
-    return s;
+  s = s.subtract(cube({size:[6,100,200],center:[1,0,0]}).translate([0,0,7]))
+  s = s.translate([0,0,2]).union(cylinder({r:60,h:2,fn:FN}))
+  s = s.subtract(cylinder({r:27,h:9,fn:FN}))
+    return s.scale([1,1,1.12]);
 }
