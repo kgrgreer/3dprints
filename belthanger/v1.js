3d0a3c7263d02c520667254d65cc0ad888390366
Kevin Greer <kgrgreer@gmail.com>
2024-02-25 16:45:15 -0500

    Improved.

--- belthanger/v1.js
+++ belthanger/v1.js
@@ -16,6 +16,9 @@ function hook(y, w) {
   s = s.union(cube({size:[10,5,H]}).translate([w/2+10,20,0]));
   s = s.union(cube({size:[10,5,H]}).translate([-w/2-20,20,0]));
 
+  s = s.union(cylinder({r:10/2,h:H}).translate([w/2+15,20+9/2,0]));
+  s = s.union(cylinder({r:10/2,h:H}).translate([-w/2-15,20+9/2,0]));
+
   s = s.union(cube({size:[25,25,H]}).rotateZ(45).translate([0,-12.5,0]));
 
   s = s.translate(([0,-y,0]));
@@ -30,6 +33,7 @@ function main() {
   var s = cylinder({h: H, r: 55/2});
  s = s.subtract(cylinder({h: H, r: 37/2}));
 
+
  s = s.subtract(cube({size:[50,50,50]}).translate([0,-50,0]));
 
  var bottom = s.intersect(cube({size:[100,50,50], center:[1,0,0]}).translate([0,-50,0]));
@@ -39,10 +43,13 @@ function main() {
 
  s = top.union(bottom).union(cube({size:[9,20,H]}).translate([-55/2,-20,0]))
 
+ s = s.union(cylinder({r:9/2, h:H}).translate([23,0,0]));
+
+
  var stem =  cube({size:[9,30,H]});
 
  var c = cylinder({r:9/2, h: H}).translate([9-9/2,30-9/2,0])
- stem = stem.subtract(cube({size:[9/2,9/2,3*H]}).translate([9/2,30-9/2,0]));
+ stem = stem.subtract(cube({size:[9/2,9/2,H]}).translate([9/2,30-9/2,0]));
 
 stem = stem.union(c);
 
@@ -51,7 +58,9 @@ stem = stem.union(c);
 
  var hanger = hook(5, 60).union(hook(105/2, 40)).union(hook(100, 20));
 
- s = s.translate([-5,68,0]).union(hanger).rotateZ(90);
+ s = s.translate([-5,68,0]).union(hanger);
+
+// s = s.rotateZ(90);
 
  s = s.intersect(cube({size:[200,200,2*H], center:[1,1,0]}));
  return s;
