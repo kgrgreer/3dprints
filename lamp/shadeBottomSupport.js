12f720ac192c0689b28b3d04a471a89cc2ee7517
Kevin Greer <kgrgreer@gmail.com>
2024-03-03 09:46:11 -0500

    Make shade 1.2 instead of 1mm thick. Make holder shorter.

--- lamp/shadeBottomSupport.js
+++ lamp/shadeBottomSupport.js
@@ -5,18 +5,18 @@ const FN   = 120;
 
 
 function holder() {
-  var s = cube({size:[1000,4,5], center:[1,1,0]});
+  var s = cube({size:[1000,4,10/2.5], center:[1,1,0]});
   s = s.union(s.rotateZ(90))
 
-  s = s.union(cylinder({r:40/2,h:5}))
-  s = s.subtract(cylinder({r:33/2,h:5}))
+  s = s.union(cylinder({r:40/2,h:10/2.5}))
+  s = s.subtract(cylinder({r:33/2,h:10/2.5}))
 
   return s.translate([0,0,10]);
 }
 
 function main() {
     s = sphere({r:R+20, fn: FN});
-    var inside = sphere({r:R+20-1, fn: FN});
+    var inside = sphere({r:R+20-1.2, fn: FN});
     s = s.subtract(inside);
 
     s = s.union(holder().intersect(inside));
