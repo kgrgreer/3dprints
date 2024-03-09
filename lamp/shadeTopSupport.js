c26b069c06ab29a8fdf99c2784560558234a4910
Kevin Greer <kgrgreer@gmail.com>
2024-03-09 10:31:55 -0500

    Final version.

--- lamp/shadeTopSupport.js
+++ lamp/shadeTopSupport.js
@@ -1,31 +1,26 @@
-const R    = 40;
+const R    = 55;
 const R2   = 33/2;
-const H    = 145;
+const H    = 35;
 const FN   = 120;
 
 
 function holder() {
-  var s = cube({size:[1000,4,10], center:[1,1,0]});
+  var s = cube({size:[1000,4,8], center:[1,1,0]});
   s = s.union(s.rotateZ(90))
 
-  s = s.union(cylinder({r:40/2,h:10}))
-  s = s.subtract(cylinder({r:33/2,h:10}))
+  s = s.union(cylinder({r:40/2,h:8}))
+  s = s.subtract(cylinder({r:33/2,h:8}))
 
-  return s.translate([0,0,10]);
+return s;
 }
 
 function main() {
-        s = sphere({r:R+20, fn: FN});
-    var inside = sphere({r:R+20-1.2, fn: FN});
+    var s      = cylinder({r:148/2, h: 2*H, fn: FN});
+    var inside = cylinder({r:148/2-1.2, h: 2*H, fn: FN});
+
     s = s.subtract(inside);
-
-    s = s.scale([1,1,3]);
-    inside = inside.scale([1,1,3]);
-
     s = s.union(holder().intersect(inside));
-
-    s = s.translate([0,0,-10]);
-    s = s.intersect(cube({size:[1000,1000,R], center:[1,1,0]}))
+    s = s.intersect(cube({size:[1000,1000,H], center:[1,1,0]}))
 
     return s;
 }
